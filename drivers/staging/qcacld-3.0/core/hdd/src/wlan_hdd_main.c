ffc3862bc8b82bf53502865ff15ac3da7470c4b2
Yaroslav Furman <yaro330@gmail.com>
2020-11-22 13:36:35 +0300

    qcacld-3.0: add a few ifdefs and remove werror
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -9242,6 +9242,7 @@ void hdd_bus_bw_compute_reset_prev_txrx_stats(struct hdd_adapter *adapter)
 
 #endif /* MSM_PLATFORM */
 
+#ifdef WLAN_DEBUG
 static uint8_t *convert_level_to_string(uint32_t level)
 {
 	switch (level) {
@@ -9258,6 +9259,7 @@ static uint8_t *convert_level_to_string(uint32_t level)
 		return "INVAL";
 	}
 }
+#endif
 
 /**
  * wlan_hdd_display_tx_rx_histogram() - display tx rx histogram
